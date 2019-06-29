dba303276b78f3b0eb9b096bc3fec43cd0d4fb0b
Bohdan <koragg666@gmail.com>
2019-06-29 12:34:21 +0300

    init

---
+++ database/migrations/2019_06_29_091147_create_employees.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('employees');
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('phone', 12);
+            $table->string('email', 255);
+            $table->float('salary');
+            $table->unsignedBigInteger('position_id');
+            $table->unsignedBigInteger('head');
+            $table->unsignedBigInteger('admin_updated_id');
+            $table->unsignedBigInteger('admin_created_id');
+            $table->foreign('position_id')->references('id')->on('position');
+            $table->foreign('head')->references('id')->on('employees');
+            $table->foreign('admin_updated_id')->references('id')->on('users');
+            $table->foreign('admin_created_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
